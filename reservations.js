a933f3231234772f0f45fa9ebbd318750cb6d9ef
Vereshchynskyi Nazar <verechnazar12@gmail.com>
2025-03-31 23:05:23 +0300

    v1.0.3

---
+++ reservations.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let container = document.querySelector(".cards-container");
+    let reservedTours = JSON.parse(localStorage.getItem("reservedTours")) || [];
+
+    reservedTours.forEach(tour => {
+        let card = document.createElement("div");
+
+        card.classList.add("card");
+        card.innerHTML = `
+            <img class="${tour.image_class}" src="${tour.image}" alt="${tour.name}">
+            <h2>${tour.name}</h2>
+            <p>${tour.country}, ${tour.name}</p>
+            <p>${tour.date}</p>
+            <div class="price-and-buy">
+                <button class="delete-btn">Delete</button>
+            </div>
+        `;
+
+        card.querySelector(".delete-btn").addEventListener("click", () => {
+            deleteTour(tour.name);
+        });
+
+        container.appendChild(card);
+    });
+});
+
+function deleteTour(tourName) {
+    // let reservedTours = JSON.parse(localStorage.getItem("reservedTours")) || [];
+
+    // reservedTours = reservedTours.filter(t => t.name !== tourName);
+    // localStorage.setItem("reservedTours", JSON.stringify(reservedTours));
+
+    // location.reload();
+}
---
+++ script.js
@@ -0,0 +1,126 @@
+document.addEventListener("DOMContentLoaded", function () {
+	let tours = [
+		{
+			name: "Kotor",
+			country: "Montenegro",
+			date: "Aug 13 - Jul 20",
+			price: 850,
+			image: "images/Kotor1.png",
+			image_class: "",
+			coords: [42.4637, 18.7712]
+		},
+		{
+			name: "Zadar",
+			country: "Croatia",
+			date: "Jun 3 - Jun 10",
+			price: 700,
+			image: "images/Zadar1.png",
+			image_class: "",
+			coords: [44.1194, 15.2318]
+		},
+		{
+			name: "Ravenna",
+			country: "Italy",
+			date: "Jul 10 - Jul 17",
+			price: 750,
+			image: "images/Ravenna1.png",
+			image_class: "",
+			coords: [44.4184, 12.2047]
+		},
+		{
+			name: "Palma",
+			country: "Spain",
+			date: "Jul 10 - Jul 17",
+			price: 690,
+			image: "images/Palma1.png",
+			image_class: "",
+			coords: [39.5696, 2.6502]
+		},
+		{
+			name: "Herceg-Novi",
+			country: "Montenegro",
+			date: "Jun 3 - Jun 10",
+			price: 710,
+			image: "images/Herceg-Novi1.png",
+			image_class: "",
+			coords: [42.4584, 18.5344]
+		},
+		{
+			name: "Venice",
+			country: "Italy",
+			date: "Aug 13 - Jul 20",
+			price: 810,
+			image: "images/Venice1.png",
+			image_class: "",
+			coords: [45.4408, 12.3155]
+		},
+		{
+			name: "Marseille",
+			country: "France",
+			date: "Jul 10 - Jul 17",
+			price: 760,
+			image: "images/Marseille1.png",
+			image_class: "",
+			coords: [43.2965, 5.3698]
+		},
+		{
+			name: "Barcelona",
+			country: "Spain",
+			date: "Aug 13 - Jul 20",
+			price: 800,
+			image: "images/Barcelona1.png",
+			image_class: "",
+			coords: [41.3784, 2.1915]
+		},
+		{
+			name: "Durres",
+			country: "Albania",
+			date: "Jun 3 - Jun 10",
+			price: 850,
+			image: "images/Durres1.png",
+			image_class: "",
+			coords: [41.3231, 19.4543]
+		}
+	];	
+
+	let cardsContainer = document.querySelector(".cards-container");
+
+	tours.forEach(tour => {
+		let card = document.createElement("div");
+		tour.image_class = tour.name + "-image";
+
+		card.classList.add("card");
+		card.innerHTML = `
+			<img class="${tour.image_class}" src="${tour.image}" alt="${tour.name}">
+			<h2>${tour.name}</h2>
+			<p>${tour.country}, ${tour.name}</p>
+			<p>${tour.date}</p>
+			<div class="price-and-buy">
+				<span>${tour.price}$</span>
+				<button class="reserve-btn">Reserve</button>
+			</div>
+		`;
+
+		card.querySelector(".reserve-btn").addEventListener("click", () => {
+			reserveTour(tour);
+		});
+
+		cardsContainer.appendChild(card);
+	});
+
+});
+
+function reserveTour(tour) {
+	// let reservedTours = JSON.parse(localStorage.getItem("reservedTours")) || [];
+	
+	// if (!reservedTours.some(t => t.name === tour.name)) {
+	// 	reservedTours.push(tour);
+	// 	localStorage.setItem("reservedTours", JSON.stringify(reservedTours));
+		
+	// 	window.location.href = "bookings.html";
+	// }
+
+	// else {
+	// 	alert("Tour is already reserved!");
+	// }
+}
